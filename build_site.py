8c7ef41bf45897c68fc9b6248aee392810544601
vaisisrael <vais.israel@gmail.com>
2026-08-11 17:19:16 +0300

    Add files via upload

--- build_site.py
+++ build_site.py
@@ -858,7 +858,7 @@ def print_home_button() -> str:
 <div class="parasha-home-print">
   <a
     class="parasha-choice"
-    href="print/?autoprint=1"
+    href="print.html?autoprint=1"
     aria-label="הדפסת תקציר, וורט ועברית"
   >
     🖨️ הדפסה
@@ -1256,13 +1256,13 @@ def build(
 
 </section>
 
-{print_home_button()}
-
 <section class="cards-grid">
   {home_cards}
 </section>
 
 {whatsapp_card()}
+
+{print_home_button()}
 """
 
     write_file(
