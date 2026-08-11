b85ce48f1ad112fe802105647d838e47a2953183
vaisisrael <vais.israel@gmail.com>
2026-08-11 21:22:37 +0300

    Refresh print links after spinner fix

--- postprocess_print_cards.py
+++ postprocess_print_cards.py
@@ -14,7 +14,7 @@ def print_card(prefix: str, parasha_name: str) -> str:
         {
             "parasha": parasha_name,
             "autoprint": "1",
-            "v": "4",
+            "v": "5",
         }
     )
 
@@ -87,7 +87,7 @@ def print_card(prefix: str, parasha_name: str) -> str:
     const url = new URL("print.html", rootUrl);
     url.searchParams.set("parasha", name);
     url.searchParams.set("autoprint", "1");
-    url.searchParams.set("v", "4");
+    url.searchParams.set("v", "5");
     link.href = url.href;
   }}
 
